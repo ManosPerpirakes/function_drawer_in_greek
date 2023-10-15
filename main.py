17eada4251e5228dc7e02030d32628cf5b69555e
Εμμανουήλ Περπιράκης <139139695+ManosPerpirakes@users.noreply.github.com>
2023-10-15 17:51:46 +0300

    feature updates

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from pygame import *
 
 try:
-    angle = float(input("\nΕισαγάγετε τον συντελεστή του x: "))
-    degree = float(input("Εισαγάγετε τον εκθέτη του x: "))
-    add = (float(input("Εισγάγετε τον αριθμό που θέλετε να προσθέσετε στον τύπο της συνάρτησης: ")))
+    class Fx():
+        def __init__(self):
+            self.numoffxs = int(input("\nΠόσες συναρτήσεις θέλετε να σχεδιάσετε;"))
+            self.factors = {"angle": [], "degree": [], "add": []}
+            for i in range(self.numoffxs):
+                self.factors["angle"].append(float(input("\nΕισαγάγετε τον συντελεστή του x: ")))
+                self.factors["degree"].append(float(input("Εισαγάγετε τον εκθέτη του x: ")))
+                self.factors["add"].append((float(input("Εισγάγετε τον αριθμό που θέλετε να προσθέσετε στον τύπο της συνάρτησης: "))))
+    fxobj = Fx()
     w = display.set_mode((1500, 750))
     display.set_caption("Cf")
     x = 0
@@ -18,13 +24,16 @@ try:
     for i in range(750):
         yy.append(rect.Rect(x, y, 1, 1))
         y += 1
-    fx = []
-    x = -750
-    y = 750
-    for i in range(1500):
-        fx.append(rect.Rect(x+750, y+375, 1, 1))
-        x += 1
-        y = - ((angle * pow(x, degree))/500 + add)
+    funclist = []
+    for i in range(fxobj.numoffxs):
+        fx = []
+        x = -750
+        y = 750
+        for j in range(1500):
+            fx.append(rect.Rect(x+750, y+375, 1, 1))
+            x += 1
+            y = - (((fxobj.factors["angle"][i] * pow(x, fxobj.factors["degree"][i])) + fxobj.factors["add"][i]) / 500)
+        funclist.append(fx)
     clock = time.Clock()
     close = False
     while not close:
@@ -36,9 +45,10 @@ try:
             draw.rect(w, (0, 0, 0), i)
         for i in yy:
             draw.rect(w, (0, 0, 0), i)
-        for i in fx:
-            draw.rect(w, (0, 0, 0), i)
+        for i in funclist:
+            for j in i:
+                draw.rect(w, (0, 0, 0), j)
         display.update()
         clock.tick(60)
 except:
-    pass
+    print("Άκυρη είσοδος!")
